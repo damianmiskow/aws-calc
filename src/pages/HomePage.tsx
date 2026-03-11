9300cca85179742eb88a0924fa566b0e21ff7ab9
damianmiskow <111329721+damianmiskow@users.noreply.github.com>
2026-03-10 21:31:09 -0400

    added css and improved apperarance of the page, added option to pick operator, and improved HTML

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,54 +1,74 @@
-import { useState } from "react"
-import axios from "axios"
-import "./HomePage.css"
+import { useState } from "react";
+import axios from "axios";
+import "./HomePage.css";
 
 export function HomePage() {
-    const[num1, setNum1] = useState("")
-    const[num2, setNum2] = useState("")
-    const [result, setResult] = useState("")
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  const [math, setMath] = useState("");
+  const [result, setResult] = useState("");
 
-
-    async function handleAdd() {
-        try {
-            const response = await axios.post (
-                import.meta.env.VITE_API_URL,
-                {
-                    num1: num1,
-                    num2: num2,
-                    math: "+"
-                }
-            )
-            setResult(response.data.body)
-        } catch (error) {
-            console.log(error)
-            setResult("API Error")
-        }
+  async function handleAdd() {
+    try {
+      const response = await axios.post(import.meta.env.VITE_API_URL, {
+        num1: num1,
+        num2: num2,
+        math: math,
+      });
+      setResult(response.data.body);
+    } catch (error) {
+      console.log(error);
+      setResult("API Error");
     }
-    
-    
-    return (
-        <>
-        <div className="main">
-            <input
-            type = "number"
+  }
+
+  return (
+    <>
+      <title>AWS Calculator</title>
+      <div className="main-title">
+        <div>AWS Calclator</div>
+      </div>
+
+      <div className="main">
+        <div className="input-boxes-container">
+          <input
+            className="input-box"
+            type="number"
             placeholder="Number 1"
             value={num1}
             onChange={(e) => setNum1(e.target.value)}
-            />
-            
+          />
 
-            <input
-            value = {num2}
+          <select
+            className="input-box"
+            value={math}
+            onChange={(e) => setMath(e.target.value)}
+          >
+            <option>+</option>
+            <option>-</option>
+            <option>/</option>
+            <option>*</option>
+          </select>
+
+          <input
+            className="input-box"
+            value={num2}
             placeholder="Number 2"
             onChange={(e) => setNum2(e.target.value)}
-            type = "number"
-            />
-            <button onClick={handleAdd}>
-                Get Result
-            </button>
-
-            <h2>Result: {result}</h2>
+            type="number"
+          />
         </div>
-        </>
-    )
+
+        <div className="get-result-button-container">
+          <button className="get-result-button" onClick={handleAdd}>
+            Get Result
+          </button>
+        </div>
+
+        <div className="result-container">
+          <h2>Result: {result}</h2>
+        </div>
+      </div>
+    </>
+  );
 }
